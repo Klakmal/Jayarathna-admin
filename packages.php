1a062e9ed5ceb476749197f19a9d45c00c86174e
Klakmal <kasunlakmal.klj@gmail.com>
2016-08-24 00:33:52 +0530

    content edit (packages)

--- packages.php
+++ packages.php
@@ -28,24 +28,27 @@
                 <img src="img/candel.jpg" alt="blah" width=90% height=80% style="border-radius: 10%;">
                 </div></td>
 
-                <td><div id="one2"><p><font color ="yellow">
+                <td><div id="one2">
+                <p><font color ="white">
                 <h4>VIP BRONZE PACKAGE</h4><br>
-			    Casket<br>
-			    Embalming with the best English Preservatives<br>
-			    Delivery within Colombo City Limits<br>
-			    Supply of Stands<br>
-			    Carpet<br>
-			    Oil Lamp<br>
-			    Brass Canopy<br>
-			    04 Floral Arrangements and Special vehicle to carry floral tributes<br>
-			    "VOLVO" S80 Limousine Hearse with Personal Attendance on the day of funeral<br>
+                <ul>
+			    <li>* Casket</li>
+			    <li>* Embalming with the best English Preservatives</li>
+			    <li>* Delivery within Colombo City Limits</li>
+			    <li>* Supply of Stands</li>
+			    <li>* Carpet</li>
+			    <li>* Oil Lamp</li>
+			    <li>* Brass Canopy</li>
+			    <li>* 04 Floral Arrangements and Special vehicle to carry floral tributes</li>
+			    <li>* "VOLVO" S80 Limousine Hearse with Personal Attendance on the day of funeral</li>
+			    </ul><br>
 
-			Free of charge : Photo Album (60 photographs of the events on the funeral day)<br>
-			Photographer and Videographer will be available only on the funeral day.<br>
+			<b>Free of charge</b> : Photo Album (60 photographs of the events on the funeral day)<br>
+			Photographer and Videographer will be available only on the funeral day.<br><br>
 
-			Note :<br>
+			<b>Note </b>:<br>
 
-			    Above rates are applicable only within Colombo city limits. Additional charges incurred if city<br> limits exceeded.Orders accepted from Kurunegala, Ratnapura and Kandy branches would incur an<br> additional sum of Rs. 35,000/- per unit as extra transport charges</font>
+			    Above rates are applicable only within Colombo city limits. Additional charges incurred if city limits exceeded.Orders accepted from Kurunegala, Ratnapura and Kandy branches would incur an additional sum of Rs. 35,000/- per unit as extra transport charges</font>
 			    </p>
 
     			</div></td>
@@ -55,21 +58,98 @@
                 <td><div id="one1">coool
                 <img src="img/candel.jpg" alt="blah" width=90% height=90% style="border-radius: 10%;">
                 </div></td>
-                <td><div id="one2">coool naa</div></td>
+				    <td><div id="one2">
+				    <p><font color ="white">
+
+				    <h4>VIP SILVER PACKAGE</h4><br>
+				    * Casket with import fittings<br>
+				    * Embalming with the best English<br> * Preservatives<br>
+				    * Delivery within Colombo City Limits<br>
+				    * Supply of Stands<br>
+				    * Carpet<br>
+				    * Oil Lamp<br>
+				    * Brass Canopy<br>
+				    * 04 Floral Arrangements and Special vehicle to carry floral tributes<br>
+				    * "VOLVO" S80 Limousine Hearse with Personal Attendance on the day of funeral<br><br>
+
+				    <b>Free of charge</b> : Video coverage and Photo Album (60 photographs of the events on the day of funeral)
+
+					Photographer will be available only on the funeral day.<br><br>
+
+					<b>Note</b> : <br>
+					bove rates are applicable only within Colombo city limits. Additional charges incurred if city limits exceeded.
+					sport charges.<br>
+
+				    </font></p>
+				</div></td>
                 </tr>
 
                 <tr>
                 <td><div id="one1">coool
                 <img src="img/candel.jpg" alt="blah" width=90% height=90% style="border-radius: 10%;">
                 </div></td>
-                <td><div id="one2">coool naa</div></td>
+                <td><div id="one2">
+                	<p><font color ="white">
+
+				    <h4>VIP GOLD PACKAGE</h4><br>
+
+				   * Casket with heavy import fittings<br>
+				    * Embalming with the best English Preservatives<br>
+				    * Delivery within Colombo City Limits<br>
+				    * Supply of Stands<br>
+				    * Carpet<br>
+				    * Oil Lamp<br>
+				    * Brass Canopy<br>
+				    * 04 Large Floral Arrangements and Special vehicle to carry floral tributes<br>
+				    * S80 Volvo/Mercedes Benz Hearse according to availability with Personal Attendance on the day of funeral<br>
+				    <br>
+					<b>Free of charge </b>:<i> Video coverage and Photo Album (80 photographs of the events on the day of funeral</i>Photographer will be available only on the funeral day.<br><br>
+
+					<b>Note</b> :<br>
+
+					    Above rates are applicable only within Colombo city limits. Additional charges incurred if city limits exceeded.
+					    Orders accepted from Kurunegala, Ratnapura and Kandy branches would incur an additional sum of Rs. 35,000/- per unit as extra transport charges.<br>
+
+					   </font></p>
+
+
+                </div></td>
                 </tr>
 
                 <tr>
                 <td><div id="one1">coool
                 <img src="img/candel.jpg" alt="blah" width=90% height=90% style="border-radius: 10%;">
                 </div></td>
-                <td><div id="one2">coool naa</div></td>
+                <td><div id="one2">
+                	<p><font color ="white">
+
+				    <h4>VIP PLATINUM PACKAGE</h4><br>
+
+				    * Teak casket with heavy import fittings<br>
+				    * Embalming with the best English Preservatives<br>
+				    * Delivery within Colombo City Limits<br>
+				    * Supply of Stands<br>
+				    * Carpet<br>
+				    * Oil Lamp<br>
+				    * Brass Canopy<br>
+				    * 04 Large Floral Arrangements and Special vehicle to carry floral tributes<br>
+				    * Brand New Mercedes Benz Hearse with Personal Attendance on the day of funeral.<br><br>
+
+				    <b>Free of charge</b> :<i> Video coverage and Photo Album
+					(100 photographs of the events on the day of funeral)</i><br>
+
+					Photographer will be available only on the funeral day.<br>
+
+					Special complimentary floral arrangement for casket.<br><br>
+					<b>Note</b>:<br>
+
+					    Above rates are applicable only within Colombo city limits. Additional charges incurred if city limits exceeded.
+					    Orders accepted from Kurunegala, Ratnapura and Kandy branches would incur an additional sum of Rs. 35,000/- per unit as extra transport charges.<br>
+
+
+
+
+                </div></td>
                 </tr>
 
                 <tr>
