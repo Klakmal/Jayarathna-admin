8b94106bdb9b45644bed44c24d3894b4fbaaf1b0
Klakmal <kasunlakmal.klj@gmail.com>
2016-08-23 18:59:36 +0530

    start package page

---
+++ packages.php
@@ -0,0 +1,100 @@
+<html>
+<head>
+    <title>Condolence Screen</title>
+
+    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/menu/simple_menu.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/packages.css">
+
+</head>
+<body>
+
+<?php include 'temp/header.php';  ?>
+
+
+
+<div id="packages">
+    <form id="p1" action="packages.php" method="post">
+            <table id="tb1">
+
+                <tr>
+                    <td colspan="2">
+                        <div><h2 id="h1">Packages and services</h2></div>
+                    </td>
+                </tr>
+
+                <tr>
+                <td><div id="one1">Rs.275, 000.00
+                <img src="img/candel.jpg" alt="blah" width=90% height=90% style="border-radius: 10%;">
+                </div></td>
+
+                <td><div id="one2"><p><font color ="yellow">
+                <h4>VIP BRONZE PACKAGE</h4><br>
+			    Casket<br>
+			    Embalming with the best English Preservatives<br>
+			    Delivery within Colombo City Limits<br>
+			    Supply of Stands<br>
+			    Carpet<br>
+			    Oil Lamp<br>
+			    Brass Canopy<br>
+			    04 Floral Arrangements and Special vehicle to carry floral tributes<br>
+			    "VOLVO" S80 Limousine Hearse with Personal Attendance on the day of funeral<br>
+
+			Free of charge : Photo Album (60 photographs of the events on the funeral day)<br>
+			Photographer and Videographer will be available only on the funeral day.<br>
+
+			Note :<br>
+
+			    Above rates are applicable only within Colombo city limits. Additional charges incurred if city<br> limits exceeded.Orders accepted from Kurunegala, Ratnapura and Kandy branches would incur an<br> additional sum of Rs. 35,000/- per unit as extra transport charges</font>
+			    </p>
+
+    			</div></td>
+                </tr>
+
+                <tr>
+                <td><div id="one1">coool</div></td>
+                <td><div id="one2">coool naa</div></td>
+                </tr>
+
+                <tr>
+                <td><div id="one1">coool</div></td>
+                <td><div id="one2">coool naa</div></td>
+                </tr>
+
+                <tr>
+                <td><div id="one1">coool</div></td>
+                <td><div id="one2">coool naa</div></td>
+                </tr>
+
+                <tr>
+                <td><div id="one1">coool</div></td>
+                <td><div id="one2">coool naa</div></td>
+                </tr>
+                 
+                 <tr>
+                <td colspan="2"><div id="h1">coool</div></td>
+                
+                </tr>   
+               
+                
+                
+                </table>
+    </form>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+<div >
+    <!-- include footer -->
+    <?php include 'temp/footer.php';  ?>
+    </body>
+</html>
