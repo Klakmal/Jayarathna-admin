78a5dde36474ec072fb889e7fc70615fea4fda70
amaPerera <maiperera93@gmail.com>
2016-08-24 17:47:44 +0530

    update contact us informations

--- contactUs.php
+++ contactUs.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!DO,CTYPE html>
 
 <html>
 	<head>
@@ -19,12 +19,39 @@
 
 	<h1> <center>Contact Us</center></h1>
 	<ul class ="a">
-	  <li> > Jayaratne AGRO TEC & NURSERIES (PVT)LTD</li>
-	  <li> > Jayaratne Stud and Agricultural Farms (PVT)Ltd</li>
-	  <li> > Jayaratne IFD</li>
-	  <li> > J.Orchids</li>
-	  <li> > Jayaratne Group of companies</li>
-	  <li> > Jayaratne Stables</li>
+	<li><h4> > Jayaratne Funeral Directors (PVT)LTD</h4></li>
+	<p>* 2B,Elvitigala Mawatha,Colombo 8.<br>Tel:2679930/31,2686981<br>Fax:2686990/2671053<br>E-mail:jvip@sltnet.lk,jvip2@yahoo.com<br>
+	Website:www.jayaratnefunerals.com<br></p>
+	<p>* 292,Hospital Road,Kalubowila.<br>Tel:2767747,2766356<br></p>
+	<p>* 319,Colombo Road,Kurunagala.<br>Tel:037-2222334,037-2226856<br>Fax:037 2226856</p>
+	<p>* 682, Peradeniya Road,Kandy<br>Tel:081-2389898,081-2389901<br></p>
+	<p>* 434,Colombo Road,Negombo.<br>Tel:031-2226565<br></p>
+	<p>* 364,Colombo Road,Rathnapura<br>Tel:045-2224200</p>
+	
+	
+	<li><h4> > Jayaratne AGRO TEC & NURSERIES (PVT)LTD</h4></li>
+	<p>* 198/1 NAWALA RD, NAWALA, WESTERN PROVINCE, SRI LANKA.<br>Tel:2805426,2671051,4547591</p>
+
+	<li> <h4>> Jayaratne Stud and Agricultural Farms (PVT)Ltd</h4></li>
+	<p>* 202, Nawala Road, Nawala, Sri Lanka, Sri Lanka.<br>Tel:2805426,2671051<br>Fax:2686900,4547591</p>
+	
+	<li> <h4>> JFD Services(PVT)LTD</h4></li>
+	<p>* 2B,Elvitigala Mawatha,Colombo 8.<br>	Tel:2679930/31,2686981<br>Fax:2686990<br>E-mail:jvip@sltnet.lk,jvip2@yahoo.com<br>
+	Website:www.jayaratnefunerals.com</p>
+	
+	<li> <h4>> J.Orchids</h4></li>
+	<p>* 292,Hospital Road,Kalubowila.<br>Tel:2766355</p>
+	
+	<li> <h4>> Jayaratne Stables</h4></li>
+	<p>* 5C Race Course, Nuwara Eliya.</p>
+	<p>* No198/1,Nawala Road,Nawala.</p>
+	
+	<li> <h4>> Jayaratne Florists (Pvt)Ltd</h4></li>
+	<p>* 83/B Negombo Road, Wattala.<br>Tel:077-3297377</p>
+	  
+	<li> <h4>> JAYARATNE VIP SERVICES (PVT)LTD</h4></li>
+	<p>* 2B,Elvitigala Mawatha,Colombo 8.<br>Tel:2679930/31,2686981,7667731,2671051<br>Fax:2686990/2671053<br>E-mail:jvip@sltnet.lk<br>
+	Website:www.jayaratnefunerals.com</p>
 	</ul>
 		</div>
 	</div>
