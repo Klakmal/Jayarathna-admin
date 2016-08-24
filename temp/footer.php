3a5a7b9a5632048a1ed0eba2350c6c3e856747de
amaPerera <maiperera93@gmail.com>
2016-08-24 14:54:18 +0530

    link contact us to the link

--- temp/footer.php
+++ temp/footer.php
@@ -18,7 +18,7 @@
       </ul>
     </div>
     <div class="one-fourth">
-      <h3>Contact us</h3>
+      <h3><a href="contactUs.php" style="color:gray;">Contact us</a></h3>
       <ul style="font-family:;, 'Times New Roman'; margin: 0 15px 0 0">
         <li style="border-bottom: 1px dotted #737a84; border-top: 1px dotted #737a84; padding: 3px 20px; letter-spacing: 2px"><a href="#" style="color:gray;">+94112686981</a></li>
         <li style="border-bottom: 1px dotted #737a84; padding: 3px 20px; letter-spacing: 2px"><a  href="#" style="color:gray;">+94112686999</a></li>
