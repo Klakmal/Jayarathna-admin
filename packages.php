52d07409035531c4b3f27fd218822debd5b8437d
Klakmal <kasunlakmal.klj@gmail.com>
2016-08-24 12:50:41 +0530

    content edit2 (packages)

--- packages.php
+++ packages.php
@@ -19,13 +19,13 @@
 
                 <tr>
                     <td colspan="2">
-                        <div><h2 id="h1">Packages and services</h2></div>
+                        <div><h2 id="h1"><font color ="yellow">Packages and services</font></h2></div>
                     </td>
                 </tr>
 
                 <tr>
-                <td><div id="one1"><p><font color ="yellow">Rs.275, 000.00</font></p>
-                <img src="img/candel.jpg" alt="blah" width=90% height=80% style="border-radius: 10%;">
+                <td><div id="one1"><p><font color ="yellow">Rs.275, 000.00<br></font></p>
+                <img src="img/candel.jpg" alt="blah" width=90% height=70% style="border-radius: 10%;">
                 </div></td>
 
                 <td><div id="one2">
@@ -56,7 +56,7 @@
 
                 <tr>
                 <td><div id="one1">coool
-                <img src="img/candel.jpg" alt="blah" width=90% height=90% style="border-radius: 10%;">
+                <img src="img/candel.jpg" alt="blah" width=90% height=70% style="border-radius: 10%;">
                 </div></td>
 				    <td><div id="one2">
 				    <p><font color ="white">
@@ -86,7 +86,7 @@
 
                 <tr>
                 <td><div id="one1">coool
-                <img src="img/candel.jpg" alt="blah" width=90% height=90% style="border-radius: 10%;">
+                <img src="img/candel.jpg" alt="blah" width=90% height=70% style="border-radius: 10%;">
                 </div></td>
                 <td><div id="one2">
                 	<p><font color ="white">
@@ -118,7 +118,7 @@
 
                 <tr>
                 <td><div id="one1">coool
-                <img src="img/candel.jpg" alt="blah" width=90% height=90% style="border-radius: 10%;">
+                <img src="img/candel.jpg" alt="blah" width=90% height=70% style="border-radius: 10%;">
                 </div></td>
                 <td><div id="one2">
                 	<p><font color ="white">
@@ -146,23 +146,123 @@
 					    Above rates are applicable only within Colombo city limits. Additional charges incurred if city limits exceeded.
 					    Orders accepted from Kurunegala, Ratnapura and Kandy branches would incur an additional sum of Rs. 35,000/- per unit as extra transport charges.<br>
 
-
+					    </font></p>
 
 
                 </div></td>
                 </tr>
 
+				<tr>
+                    <td colspan="2">
+                        <div id="h2"><font color ="white"><br>&emsp;&emsp; &emsp;Please note the following package is for funerals held at our newly renovated parlour.</font></div>
+                    </td>
+                </tr>
+
+
                 <tr>
-                <td><div id="one1">coool
-                <img src="img/candel.jpg" alt="blah" width=90% height=90% style="border-radius: 10%;">
+                <td><div id="one1">
+                <p><font color ="white">
+                	&emsp;A<br>
+
+					Rs.150, 000.00<br>
+
+					&emsp;B<br>
+
+					175,000.00<br>
+
+					&emsp;+<br>
+
+					Rs.10, 000.00<br>
+
+					Parlour charges<br>
+
+					(For day 1)<br>
+
+					(10,000/= for day 2, 7500/= for each day after)<br>
+
+					&emsp;+<br>
+
+					2 % N.B.T<br>
+
+
+                </font></p>
+
+                
                 </div></td>
-                <td><div id="one2">coool naa</div></td>
+
+
+
+                <td><div id="one2">
+                	<p><font color ="white">
+
+				    <h4>DELUXE PACKAGE</h4><br>
+
+				    * Newly renovated Parlour<br>
+				    * Casket with import fittings<br>
+				    * Embalming with English preservatives.<br>
+				    * Fresh flower arrangements<br>
+				    * Luxury Grey Volvo 960 Hearse or Vauxhall Omega Hearse<br>
+
+
+                </div></td>
                 </tr>
                  
                  <tr>
-                <td colspan="2"><div><h2 id="h2">services</h2></div></td>
+                <td colspan="2"><div><h2 id="h3"><font color ="white">&emsp;&emsp;Services</h2></font></div></td>
                 
-                </tr>   
+                </tr> 
+
+                <tr>
+                <td colspan="2"><div id="h4">
+                	<p><br><font color ="white">
+                		We are known for our varieties of services and facilities to those who place their loved ones final journey in our hands.<br><br>
+
+						We supply our customers a range of services depending on the casket of their choice, hens being experienced in the trade of funeral directing for over 50 years we offer our customers the most courteous service from professionals maintaining excellence in service. As serenity is experienced in death, we also put forward a fleet of luxurious vehicles which includes Mercedes Benz, Volvo, Ford Falcon, Fords LTD Cadillac, Toyota Crown and in addition to those lavish vehicles we also provide family cars and special vehicle to carry floral tributes and Ransivige on request.<br><br>
+
+						Settling for none less than the best we also offer our privileged customer an advanced planning or pre arrangement service which enables those to set aside funds and pre plan their funeral or their loved ones.<br><br>
+
+						Being a wide spread organization, we provide showroom facilities with a range of exquisite caskets for your choice and also space and comfort with our elegant funeral parlors. And also, since death is unexpected, we provide the facility of storing your beloved’s ashes until monumental plaques have been prepared. All these services are provided to our customers where they are comfortable to pay in any mode cash/ cheque or credit card.<br> 
+						</font>
+                	</p>
+                </div></td>
+                
+                </tr> 
+
+                <tr>
+                <td colspan="2"><div id="h4">
+                	<p><br><font color ="white"> 
+
+				              <h4>Services Available</h4><br><br>
+
+				    * Supplying Funeral requisites for any type of religious service<br>
+				    * Embalming with best quality preservatives using advanced techniques<br>
+				    * Arrangement of Floral Tributes<br>
+				    * Provision of Hymn Sheets<br>
+				    * Religious Arrangements for last respects<br>
+				    * Provision of Remembrance Booklet<br>
+				    * Assisting to hire Chairs and tents etc.<br>
+				    * Arranging Obituary Notices on the Paper and Radio announcement<br>
+				    * Crematorium Booking<br>
+				    * Engraving, Supplying and erection of Monumental Plaques<br>
+				    * Construction of different types of funeral Pyres<br>
+				    * Repatriation of Human Remaing to any country<br>
+				    * Video Coverage and Photo Album organized only for funeral day events<br>
+				    * Assisting for dress making<br>
+				    * Food and Beverage Arrangements<br><br><br>
+
+				<h4>Personalized and Special Services Available</h4><br>
+
+				Jayaratne is a resourceful leading funeral undertaker who can Provide exclusive funeral services island wide those who have obtained our service will get the following services which are distinct from other funeral directors,<br><br>
+
+				    * Supply of Imported Casket Fittings<br>
+				    * Supply of Modern and Luxury Vehicles<br>
+				    * Special Floral Arrangements<br>
+				    * Supply of best funeral requisite / items<br>
+				    * Filming and Photography for the funeral day<br>
+				    * Embalming with the best English Preservatives<br>
+				    * Personalized attention paid for the funeral from the beginning till end of funeral service.<br>
+
+                	</font></p></div></td></tr>
                
                 
                 
