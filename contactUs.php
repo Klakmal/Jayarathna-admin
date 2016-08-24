c575f50e30205ab85857075e727ecbfefdfa9b16
amaPerera <maiperera93@gmail.com>
2016-08-24 12:52:26 +0530

    contac us

---
+++ contactUs.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Jayaratne Funerals</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
+		<link rel="stylesheet" type="text/css" media="screen" href="css/menu/simple_menu.css">
+		<!--<link rel="stylesheet" type="text/css" media="screen" href="css/contactUs.css">-->
+	</head>
+
+	<body>
+	<?php include "temp/header.php"; ?>
+	
+	</br>
+	
+	<div id="para">
+
+	<h1> <center>Contact Us</center></h1>
+	<ul class ="a">
+	  <li> > Jayaratne AGRO TEC & NURSERIES (PVT)LTD</li>
+	  <li> > Jayaratne Stud and Agricultural Farms (PVT)Ltd</li>
+	  <li> > Jayaratne IFD</li>
+	  <li> > J.Orchids</li>
+	  <li> > Jayaratne Group of companies</li>
+	  <li> > Jayaratne Stables</li>
+	</ul>
+	
+	</div>
+	
+	<?php include "temp/footer.php"; ?>
+	</body>
+</html>
