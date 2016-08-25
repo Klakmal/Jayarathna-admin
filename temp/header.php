76b9cfead9e31457d6b78e07411f051e4e5d5bcc
Roshan <roshanmw91@gmail.com>
2016-08-25 12:45:29 +0530

    header change package.php

--- temp/header.php
+++ temp/header.php
@@ -2,25 +2,25 @@
   <div id="site_title"><a href="index.html"><img src="img/logo.png" alt=""></a></div>
   <!-- Dynamic Menu -->
   <ol id="menu" class="simple_menu simple_menu_css horizontal black_menu">
-    <li><a href="index.php">Home</a></li>
-    <li><a href="packeges.php">Our Services</a>
+    <li><a href="index.php">HOME</a></li>
+    <li><a href="packages.php">OUR SERVICES</a>
       <!-- sub menu -->
       <ol>
-        <li><a href="packeges.php">Packeges and Services</a></li>
-        <li><a href="reservationForm.php">Reservation Form</a></li>
-        <li><a href="prepaidplan.php">Pre-Paid Plan</a></li>
+        <li><a href="packages.php">PACKAGES AND SERVICES</a></li>
+        <li><a href="reservationForm.php">RESERVATION FORM</a></li>
+        <li><a href="prepaidplan.php">PRE-PAID PLAN</a></li>
       </ol>
       <!-- end sub menu -->
-    <li><a href="gallery.php">Gallery</a></li>
-    <li><a href="contactus.php">Contact Us</a></li>
-    <li class="last"><a href="aboutus.php">About Us</a></li>
+    <li><a href="gallery.php">GALLERY</a></li>
+    <li><a href="contactus.php">CONTACT US</a></li>
+    <li class="last"><a href="aboutus.php">ABOUT US</a></li>
   </ol>
   <div class="clr"></div>
   <!-- menu 2 -->
   <ol id="menu2" class="simple_menu simple_menu_css horizontal black_menu">
-    <li><a href="condolence.php">Condolence Screen Message</a></li>
-    <li><a href="webcast.php">Web Casting</a></li>
-    <li><a href="feedback.php">Feed-Back</a></li>
+    <li><a href="condolence.php">CONDOLENCE SCREEN MESSAGES</a></li>
+    <li><a href="webcast.php">WEB CASTING</a></li>
+    <li><a href="feedback.php">FEED-BACK</a></li>
     <li class="last" align="right" style="float:right;"><a class="abutton1" href="login.php" style="color:white;">Login/Signup</a></li>
   </ol>
   <div class="clr"></div>
--- temp/loginfooter.php
+++ temp/loginfooter.php
@@ -2,18 +2,18 @@
   <div id="site_title"><a href="index.html"><img src="img/logo.png" alt=""></a></div>
   <!-- Dynamic Menu -->
   <ol id="menu" class="simple_menu simple_menu_css horizontal black_menu">
-    <li><a href="index.php">Home</a></li>
-    <li><a href="#">Our Services</a>
+    <li><a href="index.php">HOME</a></li>
+    <li><a href="packages.php">OUR SERVICES</a>
       <!-- sub menu -->
       <ol>
-        <li><a href="packeges.php">Packeges and Services</a></li>
-        <li><a href="reservationForm.php">Reservation Form</a></li>
-        <li><a href="reservationForm.php">Pre-Paid Plan</a></li>
+        <li><a href="packages.php">PACKAGES AND SERVICES</a></li>
+        <li><a href="reservationForm.php">RESERVATION FORM</a></li>
+        <li><a href="prepaidplan.php">PRE-PAID PLAN</a></li>
       </ol>
       <!-- end sub menu -->
-    <li><a href="gallery.php">Gallery</a></li>
-    <li><a href="contactus.php">Contact Us</a></li>
-    <li class="last"><a href="aboutus.php">About Us</a></li>
+    <li><a href="gallery.php">GALLERY</a></li>
+    <li><a href="contactus.php">CONTACT US</a></li>
+    <li class="last"><a href="aboutus.php">ABOUT US</a></li>
   </ol>
   <div class="clr"></div>
   <!-- menu 2 -->
