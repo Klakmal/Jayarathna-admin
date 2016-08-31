d1fe8ce2245cc984205c0e3d7989f099346a401b
Roshan <roshanmw91@gmail.com>
2016-08-31 17:50:55 +0530

    Revert "reservation form services edit"
    This reverts commit e527d3c69aa2c3df630f45593c054ac38e0373a5.

--- reservationForm.php
+++ reservationForm.php
@@ -16,7 +16,7 @@
         require "dbcon/dbcon.php";
 
         $error=FALSE;
-            $cusnameerr = $diladderr = $dildateerr = $diltmerr = $mobilenoerr =$packname = $service1 = $qty=$qntityerr= "";
+            $cusnameerr = $diladderr = $dildateerr = $diltmerr = $mobilenoerr =$packname = $service1 =  "";
             
             if (isset($_POST['submit'])) {
                 
@@ -62,18 +62,12 @@
                 if(empty($_POST['service1'])){ 
                     $error = TRUE;
                 }else{
-                    $service1 = $_POST['service1'];
-                }
-                if(empty($_POST['service2'])){ 
-                    $error = TRUE;
-                }else{
-                    $service2 = $_POST['service2'];
+                    $packname = $_POST['service1'];
                 }
                 
                 if ($error==FALSE){
                
-                $sql = "INSERT INTO `reservations`(`cusname`, `diladd`, `dildate`, `diltime`, `mobilenum`,`packname`,`Floral tributes`,`Remembrance booklet`) VALUES ('$_POST[cusname]','$_POST[diladd]','$_POST[dildate]','$_POST[diltime]','$_POST[mobilenum]','$_POST[packname]','$_POST[service1]','$_POST[service2]')";
-
+                $sql = "INSERT INTO `reservations`(`cusname`, `diladd`, `dildate`, `diltime`, `mobilenum`,`packname`,`Floral tributes`) VALUES ('$_POST[cusname]','$_POST[diladd]','$_POST[dildate]','$_POST[diltime]','$_POST[mobilenum]','$_POST[packname]','$_POST[service1]')";
                 if(mysqli_query($conn,$sql)){
                     die();
                 } else{echo "error";}
@@ -87,7 +81,7 @@
             <table id="tb4">
                 <tr>
                     <td colspan="2">
-                        <h2 id="reshead"><font color ="#a86205">Reservation Form</h2></font>
+                        <h2 id="reshead">Reservation Form</h2>
                     </td>
                 
                 </tr>
@@ -106,66 +100,12 @@
                     
                 </tr>
                
-                <tr><td><label for="dildate">Dilivery Date</label><span class="error"></td><td  align=left  >   
-
-                    <select name=month value=''>Select Month</option>
-                    <option value='01'>January</option>
-                    <option value='02'>February</option>
-                    <option value='03'>March</option>
-                    <option value='04'>April</option>
-                    <option value='05'>May</option>
-                    <option value='06'>June</option>
-                    <option value='07'>July</option>
-                    <option value='08'>August</option>
-                    <option value='09'>September</option>
-                    <option value='10'>October</option>
-                    <option value='11'>November</option>
-                    <option value='12'>December</option>
-                    </select>
-
-
-
-                       
-
-                    <select name=dt >
-
-                    <option value='01'>01</option>
-                    <option value='02'>02</option>
-                    <option value='03'>03</option>
-                    <option value='04'>04</option>
-                    <option value='05'>05</option>
-                    <option value='06'>06</option>
-                    <option value='07'>07</option>
-                    <option value='08'>08</option>
-                    <option value='09'>09</option>
-                    <option value='10'>10</option>
-                    <option value='11'>11</option>
-                    <option value='12'>12</option>
-                    <option value='13'>13</option>
-                    <option value='14'>14</option>
-                    <option value='15'>15</option>
-                    <option value='16'>16</option>
-                    <option value='17'>17</option>
-                    <option value='18'>18</option>
-                    <option value='19'>19</option>
-                    <option value='20'>20</option>
-                    <option value='21'>21</option>
-                    <option value='22'>22</option>
-                    <option value='23'>23</option>
-                    <option value='24'>24</option>
-                    <option value='25'>25</option>
-                    <option value='26'>26</option>
-                    <option value='27'>27</option>
-                    <option value='28'>28</option>
-                    <option value='29'>29</option>
-                    <option value='30'>30</option>
-                    <option value='31'>31</option>
-                    </select>
-
-                    <input class="year" type=text name=year size=4 value="" pattern="^\d{4}$" maxlength="4" max="4" placeholder="year">
-
-                    </td>
-            
+                <tr>
+                    
+                     <td><label for="dildate">Dilivery Date</label><span class="error"><?php echo $dildateerr;?></span></td>
+                        
+                       <td><input type="date" name="dildate" id="dildate" required></td>
+                </tr>
                 <tr>
                     
                      <td><label for="diltime">Dilivery Time</label><span class="error"><?php echo $diltmerr;?></span></td>
@@ -195,7 +135,7 @@
 
                 <tr>
                     <td colspan="2">
-                        <div id="p1" align="left"><h2><font color ="white" style="font-family:myfont1; font-size:28px; color: white;"><h5>Packages</h5></font></h2></div>
+                        <div id="p1" align="left"><h2><font color ="white"><h5>Packages</h5></font></h2></div>
                     </td>
                 </tr>
 
@@ -253,48 +193,17 @@
 
 
                 <tr>
-                <td colspan="2" >
-                    <div id="p1" align="left"><h2><font color ="white" style="font-family:myfont1; font-size:28px; color: white;"><h5>Other Services</h5></font></h2></div>
+                <td colspan="2">
+                    <div id="p1" align="left"><h2><font color ="white"><h5>Other Services</h5></font></h2></div>
                 </td>
                 </tr>
 
                     <tr>
                         <td colspan="2">
                             
-                            <div id="pa2"><font color ="white"><label for="service1">-> Arrangement of Floral Tributes</label><input type="radio" name="service1" value="true" ></font></div>
-                            
+                            <div id="pa2"><font color ="white"><label for="service1">Arrangement of Floral Tributes</label><input type="radio" name="service1" value="true" ></font></div>
                         </td>
                     </tr>
-                    <tr><td id="td2"></td>
-                    <td>        <select name=s1  >
-                                <option value='s'>option1</option>
-                                <option value='ss'>option2</option>
-                                <option value='sss'>option3</option>
-                            </select>
-                    </td>
-                    </tr>
-                        
-                    
-
-                    <tr>
-                        <td colspan="2" >
-                            
-                            <div id="pa2"><font color ="white"><label for="service2">-> Provision of Remembrance Booklet</label></font></div>
-                        </td>
-                         
-                    </tr>
-
-                    
-                    <tr><td id="td1"></td>
-
-                        <td><label for="service2">Quantity </label><span class="error"><?php echo $qntityerr;?></span>
-                        
-                        <input type="text" name="service2" id="service2"></td>
-                        
-                    
-                        
-                    </tr>
-                    
 
                     
                 <tr>
