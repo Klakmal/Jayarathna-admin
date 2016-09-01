080c5ee5112faa38ae83de1d8e29a5800660df74
Klakmal <kasunlakmal.klj@gmail.com>
2016-09-01 17:50:40 +0530

    reservation eke drop down tikak damma

--- reservationForm.php
+++ reservationForm.php
@@ -34,19 +34,27 @@
                     $diladd = $_POST['diladd'];
                 }
 
-               if(empty($_POST['dildate'])){ 
+              /* if(empty($_POST['dildate'])){ 
                     $dildateerr = "required";
                     $error = TRUE;
                 }else{
                     $dildate = $_POST['dildate'];
-                }
+                }*/
+$month=$_POST['month'];
+$dt=$_POST['dt'];
+$year=$_POST['year'];
+$dildate="$year-$month-$dt";
                 
-                if(empty($_POST['diltime'])){ 
+                /*if(empty($_POST['diltime'])){ 
                     $diltmerr = "required";
                     $error = TRUE;
                 }else{
                     $diltime = $_POST['diltime'];
-                }
+                }*/
+$hours=$_POST['hours'];
+$mins=$_POST['mins'];
+$ampm=$_POST['ampm'];
+$diltime="$hours-$mins-$ampm";
                 
                 if(empty($_POST['mobilenum'])){ 
                     $mobilenoerr = "";
@@ -79,10 +87,25 @@
                 }else{
                     $service3 = $_POST['service4'];
                 }
+                if(empty($_POST['service5'])){ 
+                    $error = TRUE;
+                }else{
+                    $service3 = $_POST['service5'];
+                }
+                if(empty($_POST['service6'])){ 
+                    $error = TRUE;
+                }else{
+                    $service3 = $_POST['service6'];
+                }
+                if(empty($_POST['service7'])){ 
+                    $error = TRUE;
+                }else{
+                    $service3 = $_POST['service7'];
+                }
                 
                 if ($error==FALSE){
                
-                $sql = "INSERT INTO `reservations`(`cusname`, `diladd`, `dildate`, `diltime`, `mobilenum`,`packname`,`Floral tributes`,`Remembrance booklet`,`Chairs and tents`,`Obituary Notices`) VALUES ('$_POST[cusname]','$_POST[diladd]','$_POST[dildate]','$_POST[diltime]','$_POST[mobilenum]','$_POST[packname]','$_POST[service1]','$_POST[service2]','$_POST[service3]','$_POST[service4]')";
+                $sql = "INSERT INTO `reservations`(`cusname`, `diladd`, `dildate`, `diltime`, `mobilenum`,`packname`,`Floral tributes`,`Remembrance booklet`,`Chairs and tents`,`Obituary Notices`,`Crematorium booking`,`Monumental plaques`,`Funeral pyres`) VALUES ('$_POST[cusname]','$_POST[diladd]','$dildate','$diltime','$_POST[mobilenum]','$_POST[packname]','$_POST[service1]','$_POST[service2]','$_POST[service3]','$_POST[service4]','$_POST[service5]','$_POST[service6]','$_POST[service7]')";
 
                 if(mysqli_query($conn,$sql)){
                     die();
@@ -118,13 +141,152 @@
                     
                      <td><label for="dildate">Dilivery Date</label><span class="error"><?php echo $dildateerr;?></span></td>
                         
-                       <td><input type="date" name="dildate" id="dildate" required></td>
+                       <td><select name=month value='' required>
+                        <option value='01'>January</option>
+                        <option value='02'>February</option>
+                        <option value='03'>March</option>
+                        <option value='04'>April</option>
+                        <option value='05'>May</option>
+                        <option value='06'>June</option>
+                        <option value='07'>July</option>
+                        <option value='08'>August</option>
+                        <option value='09'>September</option>
+                        <option value='10'>October</option>
+                        <option value='11'>November</option>
+                        <option value='12'>December</option>
+                        </select>
+
+
+
+                           
+
+                        <select name=dt required >
+
+                        <option value='01'>01</option>
+                        <option value='02'>02</option>
+                        <option value='03'>03</option>
+                        <option value='04'>04</option>
+                        <option value='05'>05</option>
+                        <option value='06'>06</option>
+                        <option value='07'>07</option>
+                        <option value='08'>08</option>
+                        <option value='09'>09</option>
+                        <option value='10'>10</option>
+                        <option value='11'>11</option>
+                        <option value='12'>12</option>
+                        <option value='13'>13</option>
+                        <option value='14'>14</option>
+                        <option value='15'>15</option>
+                        <option value='16'>16</option>
+                        <option value='17'>17</option>
+                        <option value='18'>18</option>
+                        <option value='19'>19</option>
+                        <option value='20'>20</option>
+                        <option value='21'>21</option>
+                        <option value='22'>22</option>
+                        <option value='23'>23</option>
+                        <option value='24'>24</option>
+                        <option value='25'>25</option>
+                        <option value='26'>26</option>
+                        <option value='27'>27</option>
+                        <option value='28'>28</option>
+                        <option value='29'>29</option>
+                        <option value='30'>30</option>
+                        <option value='31'>31</option>
+                        </select>
+
+
+                       <input  type=text name=year size=4 value="" pattern="^\d{4}$" maxlength="4" max="4" placeholder="year" required></td>
+                       
                 </tr>
                 <tr>
                     
                      <td><label for="diltime">Dilivery Time</label><span class="error"><?php echo $diltmerr;?></span></td>
                         
-                       <td><input type="text" name="diltime" id="diltime" required></td>
+                       <td>
+                       <select name=hours required>
+                            <option value='00'>00</option>
+                            <option value='01'>01</option>
+                            <option value='02'>02</option>
+                            <option value='03'>03</option>
+                            <option value='04'>04</option>
+                            <option value='05'>05</option>
+                            <option value='06'>06</option>
+                            <option value='07'>07</option>
+                            <option value='08'>08</option>
+                            <option value='09'>09</option>
+                            <option value='10'>10</option>
+                            <option value='11'>11</option>
+                            <option value='12'>12</option>
+                        </select>
+                        <select name=mins required>
+                            <option value='00'>00</option>
+                            <option value='01'>01</option>
+                            <option value='02'>02</option>
+                            <option value='03'>03</option>
+                            <option value='04'>04</option>
+                            <option value='05'>05</option>
+                            <option value='06'>06</option>
+                            <option value='07'>07</option>
+                            <option value='08'>08</option>
+                            <option value='09'>09</option>
+                            <option value='10'>10</option>
+                            <option value='11'>11</option>
+                            <option value='12'>12</option>
+                            <option value='13'>13</option>
+                            <option value='14'>14</option>
+                            <option value='15'>15</option>
+                            <option value='16'>16</option>
+                            <option value='17'>17</option>
+                            <option value='18'>18</option>
+                            <option value='19'>19</option>
+                            <option value='20'>20</option>
+                            <option value='21'>21</option>
+                            <option value='22'>22</option>
+                            <option value='23'>23</option>
+                            <option value='24'>24</option>
+                            <option value='25'>25</option>
+                            <option value='26'>26</option>
+                            <option value='27'>27</option>
+                            <option value='28'>28</option>
+                            <option value='29'>29</option>
+                            <option value='30'>30</option>
+                            <option value='31'>31</option>
+                            <option value='32'>32</option>
+                            <option value='33'>33</option>
+                            <option value='34'>34</option>
+                            <option value='35'>35</option>
+                            <option value='36'>36</option>
+                            <option value='37'>37</option>
+                            <option value='38'>38</option>
+                            <option value='39'>39</option>
+                            <option value='40'>40</option>
+                            <option value='41'>41</option>
+                            <option value='42'>42</option>
+                            <option value='43'>43</option>
+                            <option value='44'>44</option>
+                            <option value='45'>45</option>
+                            <option value='46'>46</option>
+                            <option value='47'>47</option>
+                            <option value='48'>48</option>
+                            <option value='49'>49</option>
+                            <option value='50'>50</option>
+                            <option value='51'>51</option>
+                            <option value='52'>52</option>
+                            <option value='53'>53</option>
+                            <option value='54'>54</option>
+                            <option value='55'>55</option>
+                            <option value='56'>56</option>
+                            <option value='57'>57</option>
+                            <option value='58'>58</option>
+                            <option value='59'>59</option>
+                            <option value='60'>60</option>
+                        </select>
+
+                       <select name=ampm required>
+                                <option value='am'>Am</option>
+                                <option value='pm'>Pm</option>
+                            </select></td>
                 </tr>
                  <tr>
                     
@@ -222,7 +384,7 @@
                     </tr>
                     <tr>
                         <td colspan="2">
-                            <div id="pa2"><font color ="white"><label for="service2">Provision of Remembrance Booklet</label>
+                            <div id="pa2"><font color ="white"><label for="service2">Provision of Remembrance Booklet - Quantity</label>
                             <input type="text" name="service2" id="service2"></font></div>
                         </td>
                     </tr>
@@ -252,10 +414,39 @@
                             </font></div>
                         </td>
                     </tr>
+
                     <tr>
                         <td colspan="2">
-                            <div id="pa2"><font color ="white"><label for="service2">Crematorium Booking</label>
-                            <input type="radio" name="service5" id="service5"></font></div>
+                            <div id="pa2"><font color ="white"><label for="service5">Crematorium Booking</label>
+                            <input type="radio" name="service5" id="service5" value="true"></font></div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            
+                            <div id="pa2"><font color ="white"><label for="service6">Engraving, Supplying and erection of Monumental Plaques</label>
+                            <select name=service6>
+                                <option value='false'>--Choose-one--</option>
+                                <option value='op1'>option1</option>
+                                <option value='op2'>option2</option>
+                                <option value='op3'>option3</option>
+                                <option value='op4'>option4</option>
+                            </select>
+                            </font></div>
+                        </td>
+                    </tr>
+                     <tr>
+                        <td colspan="2">
+                            
+                            <div id="pa2"><font color ="white"><label for="service7">Construction of different types of funeral Pyres</label>
+                            <select name=service7>
+                                <option value='false'>--Choose-one--</option>
+                                <option value='op1'>option1</option>
+                                <option value='op2'>option2</option>
+                                <option value='op3'>option3</option>
+                                
+                            </select>
+                            </font></div>
                         </td>
                     </tr>
                     
