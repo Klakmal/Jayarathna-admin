91d5b69b571f735f714bc694ce43b180d483d424
susith <susithgunathilaka@gmail.com>
2016-09-01 14:23:22 +0530

    susith
    add interface for backend of it operator

---
+++ Admin/indexitoperator.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="css/home.css">
+</head>
+<body>
+	<div class="h1">
+		<div class="h2">
+		hako
+			<a href="condolenceadmin.php"><div class="item"></div></a>
+			
+			<a href="webcastingadmin.php"><div class="item"></div></a>
+			
+			<a href="feedbackadmin.php"><div class="item"></div></a>
+			
+			<a href="#"><div class="item"></div></a>
+			
+			<a href="#"><div class="item"></div></a>
+			
+		</div>
+		<div class="h3">
+		IT operator
+		</div>
+		<div class="h4">
+			gg well done
+		</div>
+	</div>
+</body>
+</html>
