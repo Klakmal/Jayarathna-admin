6db0411b61ff503c9dc385e9a94fd85ebf8ac958
amaPerera <maiperera93@gmail.com>
2016-11-23 15:48:16 +0530

    paypal button

--- reservationForm.php
+++ reservationForm.php
@@ -704,8 +704,10 @@
                     </tr>
                     
                     
-                <tr>
-                    <td><div id="payHere">
+                
+                <div id="payHere">
+                    <tr>
+                        <td>
                         <form action="reservationForm.php" method="POST">
                           <script
                             src="https://checkout.stripe.com/checkout.js" class="stripe-button"
@@ -719,8 +721,36 @@
                             document.getElementById("total").innerHTML = total;
                           </script>
                         </form>
-                        </div>
-                    </td>
+                        </td>
+                        <td>
+                        <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+
+                          <!-- Identify your business so that you can collect the payments. -->
+                          <input type="hidden" name="business" value="herschelgomez@xyzzyu.com">
+
+                          <!-- Specify a Buy Now button. -->
+                          <input type="hidden" name="cmd" value="_xclick">
+
+                          <!-- Specify details about the item that buyers will purchase. -->
+                          <input type="hidden" name="item_name" value="Hot Sauce-12oz. Bottle">
+                          <input type="hidden" name="amount" value="5.95">
+                          <input type="hidden" name="currency_code" value="USD">
+
+                          <!-- Display the payment button. -->
+                          <input type="image" name="submit" border="0"
+                          src="https://www.surfaceskimmers.com/wp-content/uploads/2016/10/1431389879455.png" width="200" height="50"
+                          alt="Buy Now">
+                          <img alt="" border="0" width="1" height="1"
+                          src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" >
+
+                        </form>
+                        </td>
+                    </tr>
+                </div>
+
+                    
+                
+                <tr>
                      <td colspan="2">
                         <input type="submit" value="Submit" name="submit"> 
                         <input type="reset" value="Cancle" name="cancle">
