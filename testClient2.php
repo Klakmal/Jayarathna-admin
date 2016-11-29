c6b1214548c9abb2d3976fb0dcfc8a480888c774
amaPerera <maiperera93@gmail.com>
2016-11-29 12:10:39 +0530

    try to put browse button(under construction :-1 )

--- testClient2.php
+++ testClient2.php
@@ -17,7 +17,7 @@
 			// define variables and set to empty values
 			$deadname = $link = $totalImages = "";
 			$deadnameErr = $linkErr = $totalImagesErr ="";
-			$error= False;
+			$error= False;$count = 0;
 		
 		//checking the fields are filled
 		if (isset($_POST['submit'])) {
@@ -26,12 +26,13 @@
   			} else {
 		  		$deadname = test_input($_POST["deadname"]);
 		  	}
-
+			/*
 			if (empty($_POST["link"])) {
     			$linkErr = "Link is required";$error=True;
   			} else {
 		  		$link = test_input($_POST["link"]);
 		  	}
+		  	*/
 
 		  	if (empty($_POST["totalImages"])) {
     			$totalImagesErr = "Number of images in the directory is required";$error=True;
@@ -41,14 +42,26 @@
 		  			$totalImagesErr = "Give the total number of images in numbers..!";$error=True;
 		  		}
 		  	}
+#uploading file
+			    foreach ($_FILES['files']['name'] as $i => $name) {
+			        if (strlen($_FILES['files']['name'][$i]) > 1) {
+			            if (move_uploaded_file($_FILES['files']['tmp_name'][$i], 'images/'.$name)) {
+			                $count++;
+			            }
+			        }
+			    }
+			}   
+
+			   
+#end of uploading image
+
 		  	if($error==False){
-				$sql = "INSERT INTO personalgallery (deadname, link, num_images) VALUES('$deadname', '$link', '$totalImages')";
+				$sql = "INSERT INTO personalgallery (deadname, num_images, link) VALUES('$deadname', '$totalImages', ?)";
 			}
-			if (mysqli_query($conn,$sql)){
-			}else{
-				echo "error";
-			}
+
+
 		}
+
 		
 		//securing inputs and form action
 		function test_input($data) {
@@ -61,18 +74,19 @@
 
 		?>
 
-		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" enctype="multipart/form-data">
 			Dead person's name: <input type="text" name="deadname">
 			<span class="error">* <?php echo $deadnameErr;?></span>
 			<br><br>
-			Link of image directory: <input type="text" name="link">
+			<!--Link of image directory: <input type="text" name="link">
 			<span class="error">* <?php echo $linkErr;?></span>
-			<br><br>
+			<br><br>-->
 			Number of images: <input type="text" name="totalImages" >
 			<span class="error">* <?php echo $totalImagesErr;?></span>
 			<br><br>
-			<input type="file" name="file" value="Upload Images">
-			<input type="submit" name="submit" value="Submit">
+			<input type="file" name="files[]" id="files" multiple="" directory="" webkitdirectory="" mozdirectory="">
+
+			<input type="submit" name="submit" value="Upload">
 			
 		</form>
 
