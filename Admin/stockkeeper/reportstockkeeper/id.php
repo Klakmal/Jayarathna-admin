edacd675e958bd067ef0f49000c1559fb533a6af
Rmadusanka <roshanmw91@gmail.com>
2017-01-12 10:39:49 +0530

    stckkeeper  done

--- Admin/stockkeeper/reportstockkeeper/id.php
+++ Admin/stockkeeper/reportstockkeeper/id.php
@@ -5,8 +5,8 @@
     <link rel="stylesheet" type="text/css" href="../../css/adminindex.css">
     <link rel="stylesheet" type="text/css" href="../../css/manage.css">
     <style>
-    html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
-    .w3-sidenav a,.w3-sidenav h4 {font-weight:bold}
+    html,body,h1,h2,h3,h4,h5 {font-family: 'Ruda', sans-serif;}
+    .w3-sidenav a,.w3-sidenav h4 {font-weight:bold;}
     </style>
 <style>
 body{
@@ -54,20 +54,27 @@ input[type=text]:hover,[type=password]:hover{
 </style>
 </head>
 <body>
-<nav class="navi_menu" id="mySidenav"><br>
+<nav class="navi_menu" id="mySidenav">
   <div class="container">
-    <div class="navi_pro">
-    <img class="propic" src="../../img_avatar_g2.jpg"><br>
-    <h4 class=""><b>Kasun Lakmal</b></h4>
-    <p class=""><h3>STOCK MANAGMENT</h3></p>
+    <div class="headdiv" align="center">
+        <span class="headline">JAYARATNE FUNERALS</span>
     </div>
+    <div class="navi_pro" align="center">
+    <img class="propic" src="../../img_avatar_g2.jpg"><br>
+    <p style="color:#aeb2b7;">Welcome,</p>
+    <h4 class="name"><b>Kasun Lakmal</b></h4>
+<!--    <p class="other">Jayarathna Funrels</p>-->
+        <hr>
+    </div>
+      <div class="menutitlediv">
+          <p class="menutitle">Menu</p>
+      </div>
   </div>
   <a href="../indexstockkeeper.php" class="navi"><img src="../../img/home.png" class="image">&nbsp;&nbsp;HOME</a>
-  <a href="../reportstockkeeper/id.php" class="navi">&nbsp;&nbsp;Coffin ID Registration</a>
-  <a href="../reportstockkeeper/moq.php" class="navi">&nbsp;&nbsp;Add/Change MOQ</a>
-
-  <a href="report1.php" class="navi">&nbsp;&nbsp;Stock Details</a>
-  <a href="report2.php" class="navi">&nbsp;&nbsp;Stock Count</a>
+  <a href="id.php" class="navi"><img src="../../img/report.png" class="image">&nbsp;&nbsp;COFFIN ID REGISTRATION</a>
+  <a href="moq.php" class="navi"><img src="../../img/updateprice.png" class="image">&nbsp;&nbsp;ADD/CHANGE MOQ</a>
+  <a href="report1.php" class="navi"><img src="../../img/stock.png" class="image">&nbsp;&nbsp;STOCK DETAILS</a>
+  <a href="report2.php" class="navi"><img src="../../img/account.png" class="image">&nbsp;&nbsp;STOCK COUNT</a>
   
 </nav>
 <div class="menu2" align="right">
