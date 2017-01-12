7386580988c53cd24fc917f12a777eb5072434e1
Rmadusanka <roshanmw91@gmail.com>
2017-01-12 10:01:14 +0530

    admin done

--- Admin/admin/indexadmin.php
+++ Admin/admin/indexadmin.php
@@ -5,22 +5,31 @@
     <link rel="stylesheet" type="text/css" href="../css/adminindex.css">
     <link rel="stylesheet" type="text/css" href="../css/manage.css">
     <style>
-    html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
-    .w3-sidenav a,.w3-sidenav h4 {font-weight:bold}
+    html,body,h1,h2,h3,h4,h5 {font-family: 'Ruda', sans-serif;}
+    .w3-sidenav a,.w3-sidenav h4 {font-weight:bold;}
     </style>
     </head>
     <body>
 
-<nav class="navi_menu" id="mySidenav"><br>
+<nav class="navi_menu" id="mySidenav">
   <div class="container">
-    <div class="navi_pro">
-    <img class="propic" src="../img_avatar_g2.jpg"><br>
-    <h4 class=""><b>Kasun Lakmal</b></h4>
-    <p class="">Jayarathna Funrels</p>
+    <div class="headdiv" align="center">
+        <span class="headline">JAYARATNE FUNERALS</span>
     </div>
+    <div class="navi_pro" align="center">
+    <img class="propic" src="../img_avatar_g2.jpg"><br>
+    <p style="color:#aeb2b7;">Welcome,</p>
+    <h4 class="name"><b>Kasun Lakmal</b></h4>
+<!--    <p class="other">Jayarathna Funrels</p>-->
+        <hr>
+    </div>
+      <div class="menutitlediv">
+          <p class="menutitle">Menu</p>
+      </div>
   </div>
+<a href="indexadmin.php" class="navi"><img src="../img/home.png" class="image">&nbsp;&nbsp;HOME</a>
   <a href="manage.php" class="navi"><img src="../img/stock.png" class="image">&nbsp;&nbsp;EMPLOYEE REGISTER</a>
-  <a href="../edit.php" class="navi"><img src="../img/home.png" class="image">&nbsp;&nbsp;UPDATE PROFILE</a>
+  <a href="editadmin.php" class="navi"><img src="../img/profile.png" class="image">&nbsp;&nbsp;UPDATE PROFILE</a>
   
 </nav>
 
