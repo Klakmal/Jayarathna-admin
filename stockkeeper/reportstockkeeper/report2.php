ba72adeb5ecc447eebbbd2a425e973610ab0ac02
Rmadusanka <roshanmw91@gmail.com>
2017-01-15 16:59:33 +0530

    report

--- stockkeeper/reportstockkeeper/report2.php
+++ stockkeeper/reportstockkeeper/report2.php
@@ -55,28 +55,6 @@ td{
     
 </head>
 <body>
-<?php
-require "dbcon/dbcon.php";
-
-    $result = mysqli_query($conn, "SELECT COUNT(id.id) FROM id, type, moq WHERE id.timeout > CURDATE() AND id.no = type.no AND type.type = moq.type AND type.type = 'platinum'");
-    $result = mysqli_fetch_all($result,MYSQLI_ASSOC);
-    $platinum = $result[0] ["COUNT(id.id)"];
-
-    $result = mysqli_query($conn, "SELECT COUNT(id.id) FROM id, type, moq WHERE id.timeout > CURDATE() AND id.no = type.no AND type.type = moq.type AND type.type = 'gold'");
-    $result = mysqli_fetch_all($result,MYSQLI_ASSOC);
-    $gold = $result[0] ["COUNT(id.id)"];
-
-    $result = mysqli_query($conn, "SELECT COUNT(id.id) FROM id, type, moq WHERE id.timeout > CURDATE() AND id.no = type.no AND type.type = moq.type AND type.type = 'silver'");
-    $result = mysqli_fetch_all($result,MYSQLI_ASSOC);
-    $silver = $result[0] ["COUNT(id.id)"];
-
-    $result = mysqli_query($conn, "SELECT COUNT(id.id) FROM id, type, moq WHERE id.timeout > CURDATE() AND id.no = type.no AND type.type = moq.type AND type.type = 'bronze'");
-    $result = mysqli_fetch_all($result,MYSQLI_ASSOC);
-    $bronze = $result[0] ["COUNT(id.id)"];
-
-
-
-?>
 <nav class="navi_menu" id="mySidenav">
   <?php include '../../details.php'; ?>
   <a href="../indexstockkeeper.php" class="navi"><img src="../../img/home.png" class="image">&nbsp;&nbsp;HOME</a>
@@ -135,64 +113,3 @@ require "dbcon/dbcon.php";
 </div>
 </body>
 </html>
-<script type="text/javascript">
-    
-    function createGraph(platinum,gold,silver,bronze){
-        $('#mychart1').highcharts({
-            chart: {
-                type: 'column'
-            },
-            title: {
-                text: 'Current Coffin Levels'
-            },
-            xAxis: {
-                type: 'category'
-            },
-            yAxis: {
-                title: {
-                    text: 'Number of Coffins'
-                }
-
-            },
-            legend: {
-                enabled: false
-            },
-            plotOptions: {
-                series: {
-                    borderWidth: 0,
-                    dataLabels: {
-                        enabled: true,
-                        format: '{point.y:.0f}'
-                    }
-                }
-            },
-
-            tooltip: {
-                pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.0f}</b> <br/>'
-            },
-
-            series: [{
-                colorByPoint: true,
-                data: [{name: 'Platinum',
-                        y:platinum,
-                },{
-                    name: 'Gold',
-                    y:gold,
-                }, {
-                    name: 'Silver',
-                    y:silver ,
-                },{
-                    name: 'Bronze',
-                    y:bronze,
-                }]
-            }]
-        });
-
-    }
-    var platinum = parseInt('<?php echo $platinum; ?>');
-    var gold = parseInt('<?php echo $gold; ?>');
-    var silver = parseInt('<?php echo $silver; ?>');
-    var bronze = parseInt('<?php echo $bronze; ?>');
-    createGraph(platinum,gold,silver,bronze);
-
-</script>
--- stockkeeper/stockmanagement.php
+++ stockkeeper/stockmanagement.php
@@ -30,5 +30,104 @@
       <a href="../signout.php" class="myButton">Log Out</a>
     </div>
   </div>
-  </body>
-  </html>
+        
+<div class="con1" align="center">
+<div class="con2">
+    <?php
+                    
+                require "../dbcon/dbcon.php";
+                     $error=FALSE;
+                    $iderr = $noerr = "";
+                 if (isset($_POST['update'])) {
+                     
+                     if(empty($_POST['id'])){ 
+                                $iderr = "</br>* ";
+                                $error = TRUE;
+                            }else{
+                                $id = $_POST['id'];
+                            }
+                     if(empty($_POST['no'])){ 
+                                $noerr = "</br>* ";
+                                $error = TRUE;
+                            }else{
+                                $no = $_POST['no'];
+                            }
+                 
+                 
+                if ($error==FALSE){
+
+                $sql = "UPDATE id SET no='$no' WHERE id='$id'";
+
+                if (mysqli_query($conn, $sql)) {
+                    echo '<script>window.location("Record updated successfully")</script>';
+                } else {
+                    echo "error" ;
+                }
+            }
+        }
+    
+
+                ?>
+
+                <div id="type">
+                <form method="post" action="stockmanagement.php">
+                <table id="tb6">
+                    <tr>
+                        <th colspan="2" align="left"><b style="color:white; font-size:24px; text-shadow:2px 2px 2px gray;">Coffin Types</b></th> 
+                    </tr>
+                 
+                    <tr>
+                    <td><label for="type">ID : </label><span class="error"><?php echo $iderr;?></span></td>
+                    <td><input type="text" name="id" placeholder="ID"></td>
+                    </tr>
+                    <tr>
+                    <td><label for="moq">NO : </label><span class="error"><?php echo $noerr;?></span></td>
+                    <td><input type="text" name="no" placeholder="NO"></td>
+                    </tr>
+                   
+                    
+                        <td colspan="2" align="center">
+                        <input type="submit" value="UPDATE" name="update">
+                    </td>
+                    
+                    </tr>
+                </table>
+                </form>
+                 </div>  
+<?php
+    require "../dbcon/dbcon.php";
+    $sql = "SELECT * FROM id WHERE no = 0";
+    $query=(mysqli_query($conn,$sql));
+?>
+<table>
+    <tr>
+        <th class="tb" >ID</th> 
+        <th class="tb" >Timein</th>
+    </tr>
+<?php
+    while ($row = mysqli_fetch_assoc($query)){
+         echo '<tr align="center">';
+        
+            echo '<td class="tb2">';
+            echo $row['id'];
+            echo "</td>";
+
+            echo '<td class="tb2">';
+            echo $row['timein'];
+            echo "</td>";
+        
+           
+        echo "</tr>";}
+?>
+
+    </table>
+
+</div>
+
+
+</div>
+
+</div>  
+
+</body>
+</html>
