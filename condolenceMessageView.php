ef320117b01ea1d520617f2840273d0ef2b67d77
Sasijay <sasijay17@gmail.com>
2017-01-16 21:45:16 -0800

    Final View

--- condolenceMessageView.php
+++ condolenceMessageView.php
@@ -1,25 +1,35 @@
 <html>
 <head>
-<title>Condolence Message</title>
+	<title>Condolence View screen</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
+	<link rel="stylesheet" type="text/css" media="screen" href="css/menu/simple_menu.css">
+	<link rel="stylesheet" type="text/css" media="screen" href="css/contactUs.css">
 </head>
 <body>
-   <form method="post" action="view.php">
-   <table id="tb11">
-       <tr>
-           <th colspan="2" align="left">
-               <b>Condolence Message View</b>
-            </th> 
-       </tr>
-       <tr>
-       <td><label for="deadPersonID">Dead Person Name</label></td>
-       <td><input type="text" name="deadPersonID" placeholder="Dead Person ID" required></td>
-       </tr> 
-       <tr>
-       <td colspan="2" align="center">
-       <input type="submit" value="Search" name="insert">
-       </td>
-       </tr>
-   </table>
-   </form>
+
+<div id="par1" align="center">
+		<div id="par" align="center">
+			<div >	
+				<h1><center class="contact_header" style = "font-family: myfont1; color: #a86205;"><b>Condolence messages</b></center></h1>
+				<hr class="hh1">
+			</div>
+			<div class="tb" align="center">
+				
+							<h2> Jayarathne Funeral Directors (PVT)LTD</h2>
+							<hr class="hh1"><br><br>
+							
+						<div></div>
+						<div>
+						<form method="post" action="view.php">
+							<input type="text" name="deadPersonID" placeholder="-- Dead Person ID --" required>
+							<input type="submit" value="Search" name="insert"> <br><br>
+						</form>
+					</div>	
+					<div> </div>
+					
+			</div>
+		</div>
+	</div>
     </body>
 </html>
