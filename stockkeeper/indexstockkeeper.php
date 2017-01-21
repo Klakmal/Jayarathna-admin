cfe0bd3deafbd015cdab600227a6c5558414925e
Rmadusanka <roshanmw91@gmail.com>
2017-01-21 21:15:34 +0530

    message eka

--- stockkeeper/indexstockkeeper.php
+++ stockkeeper/indexstockkeeper.php
@@ -10,6 +10,33 @@
     <style>
     html,body,h1,h2,h3,h4,h5 {font-family: 'Ruda', sans-serif;}
     .w3-sidenav a,.w3-sidenav h4 {font-weight:bold;}
+        
+        .red{
+            background-color: yellow;
+            padding: 20px;
+            text-align: center;
+            
+        }
+        .con2{
+            padding-top: 75px;
+        }
+        
+        tr,td{
+            width: 100px;
+            padding: 10px;
+        }
+        th{
+            color: white;
+        }
+        table{
+            color: #ffeded;
+        }
+        
+        .warning{
+            color: aliceblue;
+            font-weight: bolder;
+            font-size: 18px;
+        }
     </style>
     </head>
     <body>
@@ -38,5 +65,78 @@
       <a href="../signout.php" class="myButton">Log Out</a>
     </div>
   </div>
+        
+<div class="con1" align="center">
+    <div class="con2" align="left">
+        
+    
+        
+    <?php
+        
+        require "../dbcon/dbcon.php";
+        $sql1 = "SELECT type.type, COUNT(id.id) , moq.moq FROM id, type, moq WHERE (id.timeout > CURDATE() OR id.timeout='0000-00-00') AND id.no = type.no AND type.type = moq.type GROUP BY type.type";
+        $query1=(mysqli_query($conn,$sql1));
+        if ($row = mysqli_fetch_assoc($query1)){
+        $type = $row['type'];
+        $remaining = $row['COUNT(id.id)'];
+        $moq = $row['moq'];
+        
+        if ($moq > $remaining){
+        ?>
+        <div id="location" style="margin-left:280px; width: 300px; height:350px; padding-top: 20px; background-color: #f44242; color: #ffeded;">
+            <table>
+                <tr>
+                    <td colspan="3"><span class="warning">WARNING ! </span><br> Lori Talk Eka<br></td>
+                </tr>
+                <tr>
+                    <th>
+                        Type
+                    </th>
+                    <th>
+                        Remaining
+                    </th>
+                    <th>
+                        MOQ
+                    </th>
+                </tr>
+        <?php
+        $sql2 = "SELECT type.type, COUNT(id.id) , moq.moq FROM id, type, moq WHERE (id.timeout > CURDATE() OR id.timeout='0000-00-00') AND id.no = type.no AND type.type = moq.type GROUP BY type.type";
+        $query2=(mysqli_query($conn,$sql2));
+        while ($row1 = mysqli_fetch_assoc($query2)){
+        $type1 = $row1['type'];
+        $remaining1 = $row1['COUNT(id.id)'];
+        $moq1 = $row1['moq'];     
+            
+            ?>
+            
+            <tr>
+                <td>
+                    <?php echo $type1; ?>
+                </td>
+                <td>
+                    <?php echo $remaining1; ?>
+                </td> 
+                <td>
+                    <?php echo $moq1; ?>
+                </td> 
+            </tr>
+            
+      
+            
+    <?php       
+            }
+        }
+    ?>
+                </table>
+     </div>   
+    <?php       
+            }
+    ?>
+            
+    
+        
+        
+    </div>
+</div>
   </body>
   </html>
