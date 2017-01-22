36dfcafc6d3a6326a73052491c49c37e282a8d97
Klakmal <kasunlakmal.klj@gmail.com>
2017-01-22 16:46:57 +0530

    coffine table styles

--- report/payment.php
+++ report/payment.php
@@ -45,12 +45,14 @@ input[type=text]:hover,[type=password]:hover{
 
 .tbl {
     border-collapse: collapse;
-    width: 700px;
+    width: 600px;
+    font-size: 13px;
 }
 
 .th, .td {
     text-align: left;
     padding: 8px;
+    border-bottom: 1px solid gray;
 }
 
 .tr:nth-child(even){background-color: #f2f2f2}
